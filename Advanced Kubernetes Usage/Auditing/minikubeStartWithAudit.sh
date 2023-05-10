e119d83ae35309934b4e58f58622b1804e6acc00
Alberto Mordonini <albmord@gmail.com>
2023-05-10 15:37:56 +0200

    delete subtree

--- Advanced Kubernetes Usage/Auditing/minikubeStartWithAudit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-minikube start  --extra-config=apiserver.Authorization.Mode=RBAC --extra-config=apiserver.Audit.LogOptions.Path=/var/logs/audit.log   --extra-config=apiserver.Audit.PolicyFile=/etc/kubernetes/addons/audit-policy.yaml
